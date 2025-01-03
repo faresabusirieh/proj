b740c330c36d30f0cf3ef8cd172cf6d70f026ed6
Fares abu sirieh <faresabusirieh@gmail.com>
2025-01-03 17:35:16 +0200

    js

---
+++ style.js
@@ -0,0 +1,5 @@
+const moves = document.getElementById("moves-count");
+const timeValue = document.getElementById("time");
+const starButton = document.getElementById("start");
+const stopButton = document.getElementById("stop");
+const gamContainer = document.querySelector("game-container");
