6f9c9983e170058e4e8ce41b515cc13e53598ddd
Fares abu sirieh <faresabusirieh@gmail.com>
2025-01-03 18:47:16 +0200

    fares       u

--- style.js
+++ style.js
@@ -3,3 +3,16 @@ const timeValue = document.getElementById("time");
 const starButton = document.getElementById("start");
 const stopButton = document.getElementById("stop");
 const gamContainer = document.querySelector("game-container");
+const result = document.getElementById("result");
+const controls = document.querySelector(".controls-containar");
+let cards;
+let interval;
+let firstCard = false;
+let secondCard = false;
+
+//items array
+cost items = [
+    {name :"24000",image:"24000.jpeg"},{name:"56",image:56.jpeg}
+    {name :"5",image:"5.jpeg"},{name:"2224400",image:2224400.jpeg}
+    {name :"Dimensity 7200 Ultra 5",image:Dimensity 7200 Ultra 5.jpeg}
+];I
