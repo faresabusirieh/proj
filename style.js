32ccacfcdd2edb76f8fce909c9885bac2fdaa7cf
Fares abu sirieh <faresabusirieh@gmail.com>
2025-01-03 19:33:36 +0200

    fafafa

--- style.js
+++ style.js
@@ -11,8 +11,30 @@ let firstCard = false;
 let secondCard = false;
 
 //items array
-cost items = [
-    {name :"24000",image:"24000.jpeg"},{name:"56",image:56.jpeg}
-    {name :"5",image:"5.jpeg"},{name:"2224400",image:2224400.jpeg}
-    {name :"Dimensity 7200 Ultra 5",image:Dimensity 7200 Ultra 5.jpeg}
-];I
+const items = [
+    {name :"24000",image:24000.jpeg},{name:"56",image:56.jpeg},
+    {name :"5",image:5.jpeg},{name:"2224400",image:2224400.jpeg},
+    {name :"15",image: 15.jpeg}
+];
+
+//Initial time 
+let seconds = 0,
+ minutes = 0;
+ //Initial moves and win count 
+let movesCount = 0,
+ winCount = 0;
+
+ //for timer
+ const timeGanerator = () => {
+seconds += 1;
+//minutes logic
+if (seconds >= 60) {
+minutes += 1;
+seconds = 0
+}
+ };
+ //format time before displaying 
+ let secondsValue = seconds < 10? '0${seconds}' :
+ seconds; 
+ let minutesValues = minutes <10 ? '0$(minutes)' :
+ minutes;
